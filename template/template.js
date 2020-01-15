92108dc8be3ec0bfcbd50521a918611506c260b6
Aishit Dharwal <aishitdharwal@gmail.com>
2020-01-15 20:52:47 +0530

    new

--- template/template.js
+++ template/template.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
 	$(".open").click(function(){
 		$(".menu").css("bottom","0");
 		$(".col").css("bottom","0");
+		$(".menu_list li").fadeIn();
 	});
 	$(".Close").click(function(){
 		$(".menu").css("bottom","80vh");
+		$(".menu_list li").fadeOut();
 	});
 });
